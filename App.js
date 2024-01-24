32a95ad8d5288b60daa82fab8b13fd63dc90732e
Tejas Ranbawale <92566380+tejasranbawale@users.noreply.github.com>
2024-01-24 13:21:00 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>HEllo WOrld</Text>
+    </View>
+  );
+}
+
+export default App;
